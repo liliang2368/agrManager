55fd4205563be0ecadd22c48c3c60dca25a7cac7
liyang <2363582258@qq.com>
2020-09-29 17:13:56 +0800

    启动bug修改

--- src/main/java/com/ly/agrManader/AgrManagerApplication.java
+++ src/main/java/com/ly/agrManader/AgrManagerApplication.java
@@ -1,9 +1,11 @@
 package com.ly.agrManader;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ly.agrManader.dao")
 public class AgrManagerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ly/agrManader/config/AppConfig.java
+++ src/main/java/com/ly/agrManader/config/AppConfig.java
@@ -1,32 +1,33 @@
 package com.ly.agrManader.config;
 
-import org.jboss.logging.Logger;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import redis.clients.jedis.Jedis;
-
-@Configuration
-@ComponentScan(basePackages = "com.example")
-@EnableTransactionManagement // 查找spring 托管Bean 上是否有
-// @Transactional注解,如果有，则启用事务管理器
-@PropertySource("classpath:/properties/redis.properties")
-public class AppConfig {
-	private Logger log = Logger.getLogger(AppConfig.class);
-	@Value("${redis.hostName}")
-	private String host;
-	@Value("${redis.port}")
-	private Integer port;
-
-	//将返回值的结果交给spring容器进行管理,如果以后想要使用该对象则可以直接注入.
-	@Bean
-	public Jedis jedis() {
-
-		return new Jedis(host, port);
-	}
-
-
-}
+//import redis.clients.jedis.Jedis;
+//
+//
+//@Configuration
+//@ComponentScan(basePackages = "com.ly")
+//@EnableTransactionManagement // 查找spring 托管Bean 上是否有
+//// @Transactional注解,如果有，则启用事务管理器
+//@PropertySource("classpath:/properties/redis.properties")
+//public class AppConfig {
+////	private Logger log = Logger
+//	@Value("${redis.hostName}")
+//	private String host;
+//	@Value("${redis.port}")
+//	private Integer port;
+//
+//	//将返回值的结果交给spring容器进行管理,如果以后想要使用该对象则可以直接注入.
+//	@Bean
+//	public Jedis jedis() {
+//
+//		return new Jedis(host, port);
+//	}
+//
+//
+//}

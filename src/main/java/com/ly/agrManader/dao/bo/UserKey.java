61b9f5dc2e559e2f61963853d1d0bb557102ef40
liyang <2363582258@qq.com>
2021-02-05 17:12:43 +0800

    需求表,产品表,用户表-暂时提交

---
+++ src/main/java/com/ly/agrManader/dao/bo/UserKey.java
@@ -0,0 +1,23 @@
+package com.ly.agrManader.dao.bo;
+
+public class UserKey {
+    private String host;
+
+    private String user;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}

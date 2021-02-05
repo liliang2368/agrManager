f765bae9d0306f7679433333db450282deed654b
liyang <2363582258@qq.com>
2021-02-05 17:28:10 +0800

    一个页面

--- src/main/java/com/ly/agrManader/web/management/api/argManager.java
+++ src/main/java/com/ly/agrManader/web/management/api/argManager.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class argManager {
-    @RequestMapping({"", "admin"})
+    @RequestMapping({"", "zentao"})
     public String admin() {
-        return "admin";
+        return "zentao";
     }
 
     @RequestMapping("order")

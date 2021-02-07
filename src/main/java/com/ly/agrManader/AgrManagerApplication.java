cfcb8bae8b5abec04c2644e3dabf85f4ad0e98f0
liyang <2363582258@qq.com>
2021-02-07 16:54:58 +0800

    新版

--- src/main/java/com/ly/agrManader/AgrManagerApplication.java
+++ src/main/java/com/ly/agrManader/AgrManagerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.ly.agrManader.dao")
+@MapperScan("com.ly.agrManader.dao.interface")
 public class AgrManagerApplication {
 
 	public static void main(String[] args) {
